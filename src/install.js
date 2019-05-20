44014218b832abd6de1167a75271505764f63d72
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-05-20 17:34:22 -0300

    add: .version

--- src/install.js
+++ src/install.js
@@ -1,10 +1,16 @@
+import { version } from "../package.json";
+
+export const $f = {
+  version
+};
+
 export default function(Vue, opts = {}) {
-  if (this.__installed) {
-    return;
-  }
+  if (this.__installed) return;
   this.__installed = true;
 
   // const cfg = opts.config || {}
+
+  Vue.prototype.$f = $f;
 
   opts.components &&
     Object.keys(opts.components).forEach(key => {
