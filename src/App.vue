8ba2b325225ec48efe1f98d4762104e1b7aa4f19
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-05-24 16:36:26 -0300

    add: new home page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,66 @@
 <template>
-  <section>
-    <div>
-      <f-button label="Botão" />
-      <f-button type="flat">Botão</f-button>
-      <f-button icon="alarm" />
-      <f-button icon="home" type="outline">Botão</f-button>
-    </div>
-  </section>
+  <f-container class="content">
+    <div class="flux-logo"><span>[</span>Flux<span>]</span>Style Guide</div>
+
+    <ul class="menu">
+      <li v-for="(item, index) in menuItems" :key="index">
+        <a target="_blank" :href="item.url">
+          {{ item.name }}
+        </a>
+      </li>
+    </ul>
+  </f-container>
 </template>
 
 <script>
-import { FButton } from "@/components/FButton";
+import { FContainer } from "@/components/FContainer";
 
 export default {
   components: {
-    FButton
-  }
+    FContainer
+  },
+  data: () => ({
+    menuItems: [
+      {
+        name: "Documentation",
+        url: "https://github.com/doc88git/flux-style-guide"
+      },
+      {
+        name: "Widget Base",
+        url: "https://github.com/doc88git/flux-widget-base"
+      },
+      {
+        name: "Flux-CLI",
+        url: "https://github.com/doc88git/flux-cli"
+      },
+      {
+        name: "Doc88",
+        url: "http://doc88.com.br"
+      }
+    ]
+  })
 };
 </script>
 
 <style lang="scss" src="@/assets/f-style-guide.scss"></style>
+
+<style lang="scss">
+.content {
+  @apply flex flex-wrap mx-auto items-center justify-center h-screen;
+  .flux-logo {
+    @apply w-full text-center m-0 text-5xl text-gray-500;
+    span {
+      @apply text-green-300 text-6xl mx-2;
+    }
+  }
+  ul.menu {
+    @apply w-full text-center uppercase;
+    li {
+      @apply text-gray-700 inline mx-10;
+      a:hover {
+        @apply text-gray-600;
+      }
+    }
+  }
+}
+</style>
