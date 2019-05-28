9563607a39864dd9219d98da252f3c2024ff4ffe
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-05-28 18:32:03 -0300

    fix: export components

--- src/components.js
+++ src/components.js
@@ -1,3 +1,6 @@
-import * as components from "./components/index";
-
-export default components;
+export * from "./components/FIcon/index.js";
+export * from "./components/FContainer/index.js";
+export * from "./components/FButton/index.js";
+export * from "./components/FField/index.js";
+export * from "./components/FLayout/index.js";
+export * from "./components/FList/index.js";
