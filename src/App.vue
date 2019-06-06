15ad23e3ab6f23320bfacb4e011af45ea8028921
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-06-06 15:40:46 -0300

    change: clear home

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,6 @@
   <f-container class="content">
     <div class="flux-logo"><span>[</span>Flux<span>]</span>Style Guide</div>
 
-    <f-card
-      header-bg="https://altitude-community.aircanada.com/app/webroot/img/default-bg-card.png"
-    >
-      <f-card-title>Where does it come from?</f-card-title>
-      <f-card-body>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </f-card-body>
-      <f-card-separator />
-      <f-card-actions>
-        <f-button label="Ver mais" bigger></f-button>
-        <f-button label="Ver mais" outline></f-button>
-        <f-button label="Cancelar" color="red"></f-button>
-      </f-card-actions>
-    </f-card>
-
     <ul class="menu">
       <li v-for="(item, index) in menuItems" :key="index">
         <a target="_blank" :href="item.url">
