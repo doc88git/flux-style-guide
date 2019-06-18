34af97130c34c2fbb0d52dc885274ae4cb30e13f
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-06-18 14:55:58 -0300

    add: i18n

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,15 +1,29 @@
-import Vue from 'vue';
-import VeeValidate from "vee-validate";
 import { configure, addDecorator } from '@storybook/vue';
 import { withInfo, setDefaults } from 'storybook-addon-vue-info'
 import { withKnobs } from '@storybook/addon-knobs';
 
-Vue.use(VeeValidate);
+import Vue from 'vue';
+import VeeValidate from "vee-validate";
+import VueI18n from 'vue-i18n'
+import messages from '../src/locales'
 
 import '@/assets/f-style-guide.scss'
 
+Vue.use(VeeValidate);
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'pt-br',
+  fallbackLocale: 'pt-br',
+  messages
+})
+
 addDecorator(withInfo)
 addDecorator(withKnobs)
+addDecorator(() => ({
+  template: '<story/>',
+  i18n,
+}));
 
 setDefaults({
   header: false
