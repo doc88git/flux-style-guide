24f0947c95ab4e7bc47580e686d50a9aebbfe4e2
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-06-24 18:38:09 -0300

    add: tab

--- src/components/FTab/FTab.vue
+++ src/components/FTab/FTab.vue
@@ -1,10 +1,55 @@
 <template>
-  <div class="f-tab">Tab</div>
+  <div class="f-tab">
+    <FCard>
+      <FButtonGroup
+        :tab="!fill"
+        :options="options"
+        :default="1"
+        @change="setSelected"
+      />
+      <FCardSeparator />
+      <FCardBody
+        v-for="(item, index) in options"
+        :key="index"
+        :data-value="item.value"
+      >
+        <template v-if="item.value === selected">
+          <slot :name="`content-${item.value}`" />
+        </template>
+      </FCardBody>
+    </FCard>
+  </div>
 </template>
 
 <script>
+import { FCard, FCardBody, FCardSeparator } from "../FCard";
+import { FButtonGroup } from "../FButton";
+
 export default {
-  name: "f-tab"
+  name: "f-tab",
+  components: {
+    FCard,
+    FCardBody,
+    FCardSeparator,
+    FButtonGroup
+  },
+  props: {
+    options: Array,
+    fill: Boolean
+  },
+  computed: {
+    isFill() {
+      return this.fill;
+    }
+  },
+  data: () => ({
+    selected: null
+  }),
+  methods: {
+    setSelected(id) {
+      this.selected = id;
+    }
+  }
 };
 </script>
 
--- src/components/FTab/stories/f-tab.stories.js
+++ src/components/FTab/stories/f-tab.stories.js
@@ -1,13 +1,62 @@
 import { storiesOf } from "@storybook/vue";
 import FTab from "../FTab.vue";
 
-storiesOf("Components|Tab", module).add("Default", () => ({
-  components: { FTab },
-  data: () => ({}),
-  props: {},
-  template: `
+storiesOf("Components|Tab", module)
+  .add("Default", () => ({
+    components: { FTab },
+    data: () => ({
+      options: [
+        {
+          label: "Produtos",
+          value: 1
+        },
+        {
+          label: "Vendas",
+          value: 2
+        },
+        {
+          label: "Vacas",
+          value: 3
+        }
+      ]
+    }),
+    props: {},
+    template: `
     <div class="p-8">
-      <f-tab />
+      <f-tab :options="options">
+        <template slot="content-1">Content A</template>
+        <template slot="content-2">Content B</template>
+        <template slot="content-3">Content C</template>
+      </f-tab>
     </div>
   `
-}));
+  }))
+  .add("Fill", () => ({
+    components: { FTab },
+    data: () => ({
+      options: [
+        {
+          label: "Produtos",
+          value: 1
+        },
+        {
+          label: "Vendas",
+          value: 2
+        },
+        {
+          label: "Vacas",
+          value: 3
+        }
+      ]
+    }),
+    props: {},
+    template: `
+    <div class="p-8">
+      <f-tab fill :options="options">
+        <template slot="content-1">Content A</template>
+        <template slot="content-2">Content B</template>
+        <template slot="content-3">Content C</template>
+      </f-tab>
+    </div>
+  `
+  }));
