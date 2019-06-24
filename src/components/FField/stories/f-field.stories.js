3fefc65a8934fb478871fc96fc701ef59eb3f34b
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-06-24 18:08:59 -0300

    add: stories

--- src/components/FField/stories/f-field.stories.js
+++ src/components/FField/stories/f-field.stories.js
@@ -9,50 +9,51 @@ const summary = `
 ## Label
 `;
 
-storiesOf("Form|Field", module).add(
-  "Field Component",
-  () => ({
-    components: { FField, FInput, FButton, FAvatar },
-    data: () => ({
-      value: ""
-    }),
-    props: {
-      name: {
-        default: text("name", "name", groupId)
+storiesOf("Form|Field", module)
+  .add(
+    "Basic",
+    () => ({
+      components: { FField, FInput, FButton, FAvatar },
+      data: () => ({
+        value: ""
+      }),
+      props: {
+        name: {
+          default: text("name", "name", groupId)
+        },
+        label: {
+          default: text("label", "Nome completo", groupId)
+        },
+        hint: {
+          default: text("hint", "Escreva seu nome", groupId)
+        },
+        rules: {
+          default: text("rules", "required|email", groupId)
+        },
+        hasError: {
+          default: boolean("hasError", false, groupId)
+        },
+        mask: {
+          default: text("mask", "", groupId)
+        },
+        errorMessage: {
+          default: text("errorMessage", "", groupId)
+        },
+        type: {
+          default: text("type", "text", groupId)
+        },
+        avatar: {
+          default: text(
+            "avatar",
+            "https://cdn.quasar.dev/img/avatar5.jpg",
+            groupId
+          )
+        },
+        icon: {
+          default: text("icon", "search", groupId)
+        }
       },
-      label: {
-        default: text("label", "Nome completo", groupId)
-      },
-      hint: {
-        default: text("hint", "Escreva seu nome", groupId)
-      },
-      rules: {
-        default: text("rules", "required|email", groupId)
-      },
-      hasError: {
-        default: boolean("hasError", false, groupId)
-      },
-      mask: {
-        default: text("mask", "", groupId)
-      },
-      errorMessage: {
-        default: text("errorMessage", "", groupId)
-      },
-      type: {
-        default: text("type", "text", groupId)
-      },
-      avatar: {
-        default: text(
-          "avatar",
-          "https://cdn.quasar.dev/img/avatar5.jpg",
-          groupId
-        )
-      },
-      icon: {
-        default: text("icon", "search", groupId)
-      }
-    },
-    template: `
+      template: `
       <div class="p-8">
         <f-field
           :name="name"
@@ -66,10 +67,284 @@ storiesOf("Form|Field", module).add(
         </f-field>
       </div>
       `
-  }),
-  {
-    info: {
-      summary
+    }),
+    {
+      info: {
+        summary
+      }
     }
-  }
-);
+  )
+  .add(
+    "Before",
+    () => ({
+      components: { FField, FInput, FButton, FAvatar },
+      data: () => ({
+        value: ""
+      }),
+      props: {
+        name: {
+          default: text("name", "name", groupId)
+        },
+        label: {
+          default: text("label", "Nome completo", groupId)
+        },
+        hint: {
+          default: text("hint", "Escreva seu nome", groupId)
+        },
+        rules: {
+          default: text("rules", "required|email", groupId)
+        },
+        hasError: {
+          default: boolean("hasError", false, groupId)
+        },
+        mask: {
+          default: text("mask", "", groupId)
+        },
+        errorMessage: {
+          default: text("errorMessage", "", groupId)
+        },
+        type: {
+          default: text("type", "text", groupId)
+        },
+        avatar: {
+          default: text(
+            "avatar",
+            "https://cdn.quasar.dev/img/avatar5.jpg",
+            groupId
+          )
+        },
+        icon: {
+          default: text("icon", "search", groupId)
+        }
+      },
+      template: `
+      <div class="p-8">
+        <f-field
+          :name="name"
+          :label="label"
+          :hint="hint"
+          :rules="rules"
+          :hasError="hasError"
+          :errorMessage="errorMessage"
+          >
+          <f-input placeholder="Seu nome aqui" :type="type" :mask="mask" v-model="value" />
+          <template v-slot:before>
+            <f-avatar :src="avatar" v-if="avatar" />
+          </template>
+        </f-field>
+      </div>
+      `
+    }),
+    {
+      info: {
+        summary
+      }
+    }
+  )
+  .add(
+    "After",
+    () => ({
+      components: { FField, FInput, FButton, FAvatar },
+      data: () => ({
+        value: ""
+      }),
+      props: {
+        name: {
+          default: text("name", "name", groupId)
+        },
+        label: {
+          default: text("label", "Nome completo", groupId)
+        },
+        hint: {
+          default: text("hint", "Escreva seu nome", groupId)
+        },
+        rules: {
+          default: text("rules", "required|email", groupId)
+        },
+        hasError: {
+          default: boolean("hasError", false, groupId)
+        },
+        mask: {
+          default: text("mask", "", groupId)
+        },
+        errorMessage: {
+          default: text("errorMessage", "", groupId)
+        },
+        type: {
+          default: text("type", "text", groupId)
+        },
+        avatar: {
+          default: text(
+            "avatar",
+            "https://cdn.quasar.dev/img/avatar5.jpg",
+            groupId
+          )
+        },
+        icon: {
+          default: text("icon", "search", groupId)
+        }
+      },
+      template: `
+      <div class="p-8">
+        <f-field
+          :name="name"
+          :label="label"
+          :hint="hint"
+          :rules="rules"
+          :hasError="hasError"
+          :errorMessage="errorMessage"
+          >
+          <f-input placeholder="Seu nome aqui" :type="type" :mask="mask" v-model="value" />
+          <template v-slot:after>
+            <f-button>Salvar</f-button>
+          </template>
+        </f-field>
+      </div>
+      `
+    }),
+    {
+      info: {
+        summary
+      }
+    }
+  )
+  .add(
+    "Append",
+    () => ({
+      components: { FField, FInput, FButton, FAvatar },
+      data: () => ({
+        value: ""
+      }),
+      props: {
+        name: {
+          default: text("name", "name", groupId)
+        },
+        label: {
+          default: text("label", "Nome completo", groupId)
+        },
+        hint: {
+          default: text("hint", "Escreva seu nome", groupId)
+        },
+        rules: {
+          default: text("rules", "required|email", groupId)
+        },
+        hasError: {
+          default: boolean("hasError", false, groupId)
+        },
+        mask: {
+          default: text("mask", "", groupId)
+        },
+        errorMessage: {
+          default: text("errorMessage", "", groupId)
+        },
+        type: {
+          default: text("type", "text", groupId)
+        },
+        avatar: {
+          default: text(
+            "avatar",
+            "https://cdn.quasar.dev/img/avatar5.jpg",
+            groupId
+          )
+        },
+        icon: {
+          default: text("icon", "search", groupId)
+        }
+      },
+      template: `
+      <div class="p-8">
+        <f-field
+          :name="name"
+          :label="label"
+          :hint="hint"
+          :rules="rules"
+          :hasError="hasError"
+          :errorMessage="errorMessage"
+          >
+          <f-input placeholder="Seu nome aqui" :type="type" :mask="mask" v-model="value" />
+          <template v-slot:append>
+            <f-button flat :icon="icon" />
+          </template>
+        </f-field>
+      </div>
+      `
+    }),
+    {
+      info: {
+        summary
+      }
+    }
+  )
+  .add(
+    "Complete",
+    () => ({
+      components: { FField, FInput, FButton, FAvatar },
+      data: () => ({
+        value: ""
+      }),
+      props: {
+        name: {
+          default: text("name", "name", groupId)
+        },
+        label: {
+          default: text("label", "Nome completo", groupId)
+        },
+        hint: {
+          default: text("hint", "Escreva seu nome", groupId)
+        },
+        rules: {
+          default: text("rules", "required|email", groupId)
+        },
+        hasError: {
+          default: boolean("hasError", false, groupId)
+        },
+        mask: {
+          default: text("mask", "", groupId)
+        },
+        errorMessage: {
+          default: text("errorMessage", "", groupId)
+        },
+        type: {
+          default: text("type", "text", groupId)
+        },
+        avatar: {
+          default: text(
+            "avatar",
+            "https://cdn.quasar.dev/img/avatar5.jpg",
+            groupId
+          )
+        },
+        icon: {
+          default: text("icon", "search", groupId)
+        }
+      },
+      template: `
+      <div class="p-8">
+        <f-field
+          :name="name"
+          :label="label"
+          :hint="hint"
+          :rules="rules"
+          :hasError="hasError"
+          :errorMessage="errorMessage"
+          >
+          <f-input placeholder="Seu nome aqui" :type="type" :mask="mask" v-model="value" />
+          <template v-slot:before>
+            <f-avatar :src="avatar" v-if="avatar" />
+          </template>
+          <template v-slot:after>
+            <f-button>Salvar</f-button>
+          </template>
+          <template v-slot:append>
+            <f-button flat :icon="icon" />
+          </template>
+        </f-field>
+      </div>
+      `
+    }),
+    {
+      info: {
+        summary
+      }
+    }
+  );
