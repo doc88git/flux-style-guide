913291fd001decacdacfdded3ad1b9a9724dfa35
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-06-25 09:50:15 -0300

    finx: vue - storybook integration

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,41 +2,13 @@ import { configure, addDecorator } from '@storybook/vue';
 import { withInfo, setDefaults } from 'storybook-addon-vue-info'
 import { withKnobs } from '@storybook/addon-knobs';
 
-import Vue from 'vue';
-import VeeValidate from "vee-validate";
-import VueI18n from 'vue-i18n'
-// import VuePlugin from "@/vue-plugin";
-import messages from '@/locales'
-
-import '@/assets/f-style-guide.scss'
-
-// import * as directives from "@/directives.js";
-// import * as plugins from "@/plugins.js";
-
-// VuePlugin.install(Vue, {
-//   directives,
-//   plugins
-// });
-
-// console.log({
-//   ...directives,
-//   ...plugins
-// })
-
-Vue.use(VeeValidate);
-Vue.use(VueI18n)
-
-const i18n = new VueI18n({
-  locale: 'pt-br',
-  fallbackLocale: 'pt-br',
-  messages
-})
+import vueConfig from "../vue-storybook"
 
 addDecorator(withInfo)
 addDecorator(withKnobs)
 addDecorator(() => ({
   template: '<story/>',
-  i18n,
+  i18n: vueConfig.i18n,
 }));
 
 setDefaults({
