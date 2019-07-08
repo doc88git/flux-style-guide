f4c9c64127b62fa90f9405ad1ffaaf8e71646336
Gabriel Gelado <gabriel.gelado@doc88.com.br>
2019-07-08 17:41:39 -0300

    fix: max heignt

--- src/components/FDropdown/FDropdown.vue
+++ src/components/FDropdown/FDropdown.vue
@@ -218,7 +218,8 @@ export default {
 
   &__list {
     @apply absolute m-0 pt-2 pb-2 rounded-b bg-primary cursor-pointer shadow-md w-full text-white text-left z-10 overflow-auto;
-    height: 200px;
+    height: 35px;
+    max-height: 200px;
     ul {
       @apply list-none;
       li {
