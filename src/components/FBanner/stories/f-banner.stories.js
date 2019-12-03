f34595b1986bdd0a7863b2435213f666fad57329
Gabriel Alegrete <gabriel.alegrete@doc88.com.br>
2019-12-03 20:22:46 -0300

    FBanner

--- src/components/FBanner/stories/f-banner.stories.js
+++ src/components/FBanner/stories/f-banner.stories.js
@@ -2,17 +2,22 @@ import { storiesOf } from "@storybook/vue";
 // import { boolean, text } from "@storybook/addon-knobs";
 import FBanner from "../FBanner.vue";
 
+const images = [
+  "https://www.webdesignerdepot.com/cdn-origin/uploads/2017/03/getting_started_with_vue_banner.png",
+  "https://miro.medium.com/max/1500/1*XRzDO0hdmXGQw_CmNvrL3w.jpeg"
+];
+
 storiesOf("Components|Banners", module)
   .add(
     "Banner: Slider",
     () => ({
       components: { FBanner },
-      data: () => ({}),
+      data: () => ({ images }),
       methods: {},
       props: {},
       template: `
         <div class="p-8">
-          <f-banner />
+          <f-banner :images="images"/>
         </div>
       `
     }),
@@ -25,15 +30,57 @@ storiesOf("Components|Banners", module)
     }
   )
   .add(
-    "Banner: Slider Variation",
+    "Banner: Slider with Bullet",
     () => ({
       components: { FBanner },
-      data: () => ({}),
+      data: () => ({ images }),
       methods: {},
       props: {},
       template: `
         <div class="p-8">
-          <f-banner />
+          <f-banner bullet :images="images"/>
+        </div>
+      `
+    }),
+    {
+      info: {
+        summary: `
+          # Banner
+        `
+      }
+    }
+  )
+  .add(
+    "Banner: Out Slider",
+    () => ({
+      components: { FBanner },
+      data: () => ({ images }),
+      methods: {},
+      props: {},
+      template: `
+        <div class="p-8">
+          <f-banner out-slider :images="images"/>
+        </div>
+      `
+    }),
+    {
+      info: {
+        summary: `
+          # Banner
+        `
+      }
+    }
+  )
+  .add(
+    "Banner: Out Slider with Bullet",
+    () => ({
+      components: { FBanner },
+      data: () => ({ images }),
+      methods: {},
+      props: {},
+      template: `
+        <div class="p-8">
+          <f-banner out-slider bullet :images="images"/>
         </div>
       `
     }),
