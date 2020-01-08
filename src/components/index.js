4349b66566048b5f005fe0730b0b32529e59fde2
Gabriel Alegrete <gabriel.alegrete@doc88.com.br>
2020-01-08 18:05:35 -0300

    conflicts solving

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as FIcon from './FIcon/index.js'
 import * as FContainer from './FContainer/index.js'
 import * as FButton from './FButton/index.js'
@@ -27,37 +26,7 @@ import * as FTag from './FTag/index.js'
 import * as FTagGroup from './FTagGroup/index.js'
 import * as FDatepicker from './FDatepicker/index.js'
 import * as FTablePure from './FTablePure/index.js'
-=======
-import * as FIcon from "./FIcon/index.js";
-import * as FContainer from "./FContainer/index.js";
-import * as FButton from "./FButton/index.js";
-import * as Avatar from "./Avatar/index.js";
-import * as FField from "./FField/index.js";
-import * as FLayout from "./FLayout/index.js";
-import * as FList from "./FList/index.js";
-import * as FLink from "./FLink/index.js";
-import * as FCard from "./FCard/index.js";
-import * as FImage from "./FImage/index.js";
-import * as FInput from "./FInput/index.js";
-import * as FAvatar from "./FAvatar/index.js";
-import * as FDropdown from "./FDropdown/index.js";
-import * as FPagination from "./FPagination/index.js";
-import * as FTable from "./FTable/index.js";
-import * as FBadge from "./FBadge/index.js";
-import * as FTooltip from "./FTooltip/index.js";
-import * as FSelect from "./FSelect/index.js";
-import * as FHeader from "./FHeader/index.js";
-import * as FChip from "./FChip/index.js";
-import * as FModal from "./FMoldal/index.js";
-import * as FTableCustom from "./FTableCustom/index.js";
-import * as FSeparator from "./FSeparator/index.js";
-import * as FTitle from "./FTitle/index.js";
-import * as FTag from "./FTag/index.js";
-import * as FTagGroup from "./FTagGroup/index.js";
-import * as FDatepicker from "./FDatepicker/index.js";
-import * as FTablePure from "./FTablePure/index.js";
-import * as FToggle from "./FToggle/index.js";
->>>>>>> 96680d5419357ccc036b7d8ed588b5169af07bbd
+import * as FToggle from './FToggle/index.js'
 
 const components = [
   FIcon,
@@ -89,6 +58,6 @@ const components = [
   FDatepicker,
   FTablePure,
   FToggle
-];
+]
 
 export default components
