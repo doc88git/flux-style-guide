c7662a9e07cd22093c9e6a8782979cbaac9ca47a
Gabriel Alegrete <gabriel.alegrete@doc88.com.br>
2020-01-14 16:43:09 -0300

    text-f-tablePure

--- src/components/FTablePure/FTablePure.vue
+++ src/components/FTablePure/FTablePure.vue
@@ -132,7 +132,7 @@ export default {
           }
           .f-icon {
             margin-left: 0.5rem;
-            font-size: 0.75rem;
+            font-size: var(--text-xs);
           }
         }
       }
