ffc87c2f5415db20ed9ca5d00972164aafce43a9
Gabriel Alegrete <gabriel.alegrete@doc88.com.br>
2020-01-14 15:56:11 -0300

    text-f-item

--- src/components/FList/FItem.vue
+++ src/components/FList/FItem.vue
@@ -38,7 +38,7 @@ export default {
   display: block;
   padding: 0.5rem 1rem;
   border-bottom-width: 1px;
-  font-size: 0.875rem;
+  font-size: var(--text-sm);
   &:hover,
   &:focus {
     background-color: var(--color-gray-300);
