8fa797ae6eeeea228dc13d18040b3d88dc8375d0
Gabriel Alegrete <gabriel.alegrete@doc88.com.br>
2020-01-14 16:41:03 -0300

    text-f-table

--- src/components/FTable/FTable.vue
+++ src/components/FTable/FTable.vue
@@ -180,7 +180,7 @@ export default {
           }
           .f-icon {
             margin-left: -0.5rem;
-            font-size: 0.75rem;
+            font-size: var(--text-xs);
           }
         }
       }
