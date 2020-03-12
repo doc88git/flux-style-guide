08954f38669b5f9602ed52d2651d9bac9880e1fb
gabrielalegrete <gabrielalegrete1@gmail.com>
2020-03-12 10:17:22 -0300

    f-icon-stories

--- src/components/FIcon/stories/f-icon.stories.js
+++ src/components/FIcon/stories/f-icon.stories.js
@@ -6,36 +6,70 @@ import FIcon from '../FIcon.vue'
 const label = 'Icon Type'
 const groupId = 'ICON-OPTIONS-ID1'
 
-storiesOf('Components|Icon', module).add(
-  'Icon',
-  () => ({
-    components: { FIcon },
-    props: {
-      lib: {
-        default: text('lib', 'material', groupId)
+storiesOf('Components|Icon', module)
+  .add(
+    'Icon Material',
+    () => ({
+      components: { FIcon },
+      props: {
+        lib: {
+          default: text('lib', 'material', groupId)
+        },
+        name: {
+          default: text('name', 'star', groupId)
+        },
+        color: {
+          default: text('color', 'red', groupId)
+        },
+        size: {
+          default: select(label, [16, 24], 16, groupId)
+        },
+        clickable: {
+          default: boolean('clickable', true, groupId)
+        }
       },
-      name: {
-        default: text('name', 'star', groupId)
-      },
-      color: {
-        default: text('color', 'red', groupId)
-      },
-      size: {
-        default: select(label, [16, 24], 16, groupId)
-      },
-      clickable: {
-        default: boolean('clickable', true, groupId)
-      }
-    },
-    template: `
+      template: `
       <div style="padding: 20px;">
         <FIcon :lib="lib" :color="color" :name="name" :size="size" clickable />
       </div>
     `
-  }),
-  {
-    info: {
-      summary: `FIcon Base - Flux Icon`
+    }),
+    {
+      info: {
+        summary: `FIcon Base - Flux Icon`
+      }
     }
-  }
-)
+  )
+  .add(
+    'Icon Flux',
+    () => ({
+      components: { FIcon },
+      props: {
+        lib: {
+          default: text('lib', 'flux', groupId)
+        },
+        name: {
+          default: text('name', 'star', groupId)
+        },
+        color: {
+          default: text('color', 'red', groupId)
+        },
+        size: {
+          default: select(label, [16, 24], 16, groupId)
+        },
+        clickable: {
+          default: boolean('clickable', true, groupId)
+        }
+      },
+      template: `
+      <div style="padding: 20px;">
+        <FIcon :lib="lib" :color="color" :name="name" :size="size" clickable />
+      </div>
+    `
+    }),
+    {
+      info: {
+        summary: `FIcon Base - Flux Icon`
+      }
+    }
+  )
