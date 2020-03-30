58820da476ade7f4402203e613d5dbcc1fa9bf79
Gabriel Gelado <gbgelado@gmail.com>
2020-03-30 10:37:29 -0300

    links as variables

--- src/stories/f-welcome.stories.js
+++ src/stories/f-welcome.stories.js
@@ -11,7 +11,12 @@ storiesOf(`Welcome|Flux Style Guide`, module).add('About', () => ({
   },
   data: () => ({
     version: `v${pkg.version}`,
-    today
+    today,
+    urls: {
+      doc88: 'https://doc88.com.br',
+      fsgGithub: 'https://github.com/doc88git/flux-style-guide',
+      iconsGithub: 'https://github.com/doc88git/flux-icons'
+    }
   }),
   props: {},
   methods: {},
@@ -22,10 +27,12 @@ storiesOf(`Welcome|Flux Style Guide`, module).add('About', () => ({
       </div>
       <div style="position: absolute;bottom: 50px;width: 99%;text-align: center;">
         <h6>
-          Fron-tend development team ${today.getFullYear()}
+          <a targe="_blank" :href="urls.doc88" style="font-size: var(--text-xl);">Doc88</a>
+          Front-end Development Team - ${today.getFullYear()}
         </h6>
-        <p style="font-size: 16px">
-          <a targe="_blank" href="https://doc88.com.br">Doc88</a> | <a targe="_blank" href="https://github.com/doc88git/flux-style-guide">Github</a>
+        <p style="font-size: var(--text-xl);">
+          <a targe="_blank" :href="urls.fsgGithub">Github</a> |
+          <a targe="_blank" :href="urls.iconsGithub">Flux Icons</a>
         </p>
       </div>
     </FContainer>`
