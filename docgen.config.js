90319825e6027f1d9015a5f8fe48cdcadcc543cb
Gabriel Gelado <gbgelado@gmail.com>
2020-04-06 20:00:25 -0300

    refactor

--- docgen.config.js
+++ docgen.config.js
@@ -1,39 +1,24 @@
 const path = require('path')
 
+const isDotExample = s => s.includes('.example.')
+
 module.exports = {
   componentsRoot: 'src/components',
   components: '(**|**/**)/[A-Z]*.vue',
   outDir: 'docs/.vuepress/apiComponents',
   getDocFileName: componentPath => {
     const item = componentPath.replace(/\.vue$/, '.md')
-    if (item.includes('.example.')) return ''
+    if (isDotExample(item)) return ''
+
     return item
   },
   getDestFile: (file, config) => {
     const item = path.join(config.outDir, file).replace(/\.vue$/, '.doc.md')
-    if (item.includes('.example.')) return ''
+    if (isDotExample(item)) return ''
+
     return item
   },
   templates: {
     component: require('./docgen.components.template.js')
   }
 }
-
-// const path = require('path')
-
-// module.exports = {
-//   componentsRoot: 'src/components',
-//   components: '(**|**/**)/[A-Z]*.vue',
-//   outDir: 'docs/02.components',
-//   getDocFileName: componentPath => {
-//     let fileName = componentPath.replace(/\.vue$/, '.md')
-//     return fileName.replace(/\/F/g, '/')
-//   },
-//   getDestFile: (file, config) => {
-//     let destFile = path.join(config.outDir, file).replace(/\.vue$/, '.md')
-//     return destFile.replace(/\/F/g, '/')
-//   },
-//   templates: {
-//     component: require('./docgen.components.template.js')
-//   }
-// }
