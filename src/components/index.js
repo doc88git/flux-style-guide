7a6823a470c67ae86087ae2d54020717819a0889
HarrisonVinicius <harrison_vms@hotmail.com>
2020-04-17 16:50:47 -0300

    Issue/#114 (#116)
    * implementação do componente de tags
    * correção no SCSS
    
    Co-authored-by: Harrison Vinicius <harrison.silva@doc88.com.br>
    Co-authored-by: Gabriel Gelado <gbgelado@gmail.com>

--- src/components/index.js
+++ src/components/index.js
@@ -29,6 +29,7 @@ import * as FToggle from './FToggle/index.js'
 import * as FUpload from './FUpload/index.js'
 import * as FProgressBar from './FProgressBar/index.js'
 import * as FSticky from './FSticky/index.js'
+import * as FInputTag from './FInputTag/index.js'
 import * as FAccordion from './FAccordion/index.js'
 import * as FTab from './FTab/index.js'
 
@@ -62,8 +63,9 @@ const components = [
   FDatepicker,
   FToggle,
   FUpload,
-  FSticky,
+  FInputTag,
   FProgressBar,
+  FSticky,
   FAccordion,
   FTab
 ]
