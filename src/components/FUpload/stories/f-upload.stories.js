062edfedfe4d4d45f8d070ef1a85772f7f5c58e7
Erik Isidore <ericssonico@protonmail.ch>
2020-05-06 16:50:52 -0300

    Re-write FUpload

--- src/components/FUpload/stories/f-upload.stories.js
+++ src/components/FUpload/stories/f-upload.stories.js
@@ -1,13 +1,12 @@
 import { storiesOf } from '@storybook/vue'
-// import { select, boolean } from "@storybook/addon-knobs";
 import { FUpload } from '../index.js'
-import { FButton } from '../../FButton/index.js'
 
-storiesOf('Form|Upload', module).add('Default', () => ({
-  components: { FUpload, FButton },
-  data: () => ({}),
-  props: {},
-  template: `
+storiesOf('Form|Upload', module)
+  .add('Default', () => ({
+    components: { FUpload },
+    data: () => ({}),
+    props: {},
+    template: `
       <div style="padding: 40px; width: 100%; text-align: center;">
         <f-upload>
           <f-button
@@ -21,4 +20,5 @@ storiesOf('Form|Upload', module).add('Default', () => ({
         </f-upload>
       </div>
     `
-}))
+  }))
+  .add('Multiple', () => ({}))
