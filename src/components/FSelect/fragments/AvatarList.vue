02cfeaffed5764408fc4d8a553b9f63cbaec316d
Erik Isidore <ericssonico@protonmail.ch>
2020-07-07 11:19:04 -0300

    Hotfix - FSelect AvatarList keys (#174)
    * Fix FSelect's AvatarList transition :keys
    
    * Remove index from AvatarList v-for loop

--- src/components/FSelect/fragments/AvatarList.vue
+++ src/components/FSelect/fragments/AvatarList.vue
@@ -6,8 +6,8 @@
       tag="div"
     >
       <f-tooltip
-        v-for="(avatar, index) in displayAvatars"
-        :key="index"
+        v-for="avatar in displayAvatars"
+        :key="getAvatarKey(avatar)"
         position="bottom"
         aligned="center"
         bg-color="primary"
@@ -59,6 +59,12 @@ export default {
     },
     remainingAvatars() {
       return `${(this.avatars || []).length - this.showLimit}`
+    }
+  },
+
+  methods: {
+    getAvatarKey(avatar) {
+      return JSON.stringify(avatar)
     }
   },
 
